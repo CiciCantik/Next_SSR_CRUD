c631317bb52e7b4ec8fcbf04cf95fbb25b1dbad3
CiciCantik <139205186+CiciCantik@users.noreply.github.com>
2024-07-15 00:09:49 +0700

    CRUD_SSR

---
+++ src/pages/about.jsx
@@ -0,0 +1,12 @@
+import ButtonNav from "@/components/ButtonNav";
+import Navbar from "@/components/Navbar";
+
+export default function About() {
+  return (
+    <>
+    <Navbar />
+    <h1 className="text-3xl font-medium">About Page</h1>
+    <ButtonNav/>
+    </>
+  )
+}
---
+++ src/pages/content.jsx
@@ -0,0 +1,12 @@
+import ButtonNav from "@/components/ButtonNav";
+import Navbar from "@/components/Navbar";
+
+export default function Content() {
+  return (
+    <>
+    <Navbar />
+    <h1 className="text-3xl font-medium">Content Page</h1>
+    <ButtonNav/>
+    </>
+  )
+}
